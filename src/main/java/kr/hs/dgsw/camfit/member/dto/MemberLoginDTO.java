1baca9eef11b4e340eef4caef09e621c83def4a3
seojun-kim <80928795+seojun-kim@users.noreply.github.com>
2022-10-18 17:26:23 +0900

    dto modify
    dto modify

--- src/main/java/kr/hs/dgsw/camfit/member/dto/MemberLoginDTO.java
+++ src/main/java/kr/hs/dgsw/camfit/member/dto/MemberLoginDTO.java
@@ -3,21 +3,20 @@ package kr.hs.dgsw.camfit.member.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class MemberLoginDTO {
 
-    @NotBlank
-    @Size(min = 3, max = 50)
+    @NotNull
     private String username;
 
-    @NotBlank
-    @Size(min = 3, max = 100)
+    @NotNull
     private String password;
 }
