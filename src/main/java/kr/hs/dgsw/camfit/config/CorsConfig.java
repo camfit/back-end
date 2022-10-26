387480531c127522604485cbe8c2b3392646b673
seojun-kim <80928795+seojun-kim@users.noreply.github.com>
2022-10-26 17:34:43 +0900

    cros config 수정
    cros config 수정

--- src/main/java/kr/hs/dgsw/camfit/config/CorsConfig.java
+++ src/main/java/kr/hs/dgsw/camfit/config/CorsConfig.java
@@ -10,14 +10,16 @@ import org.springframework.web.filter.CorsFilter;
 public class CorsConfig {
    @Bean
    public CorsFilter corsFilter() {
+      
       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
       CorsConfiguration config = new CorsConfiguration();
-      config.setAllowCredentials(true);
+      //config.setAllowCredentials(true);
       config.addAllowedOrigin("*");
       config.addAllowedHeader("*");
       config.addAllowedMethod("*");
 
-      source.registerCorsConfiguration("/api/**", config);
+      source.registerCorsConfiguration("/member/**", config);
+
       return new CorsFilter(source);
    }
 }
