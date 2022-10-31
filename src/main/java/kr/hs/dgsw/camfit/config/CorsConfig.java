0e95522e04fb9f6fc5d4816196f1f290668bd238
seojun-kim <80928795+seojun-kim@users.noreply.github.com>
2022-10-31 11:10:14 +0900

    cros 문제 해결
    member 외의 도메인에 접속이 불가한 문제 해결

--- src/main/java/kr/hs/dgsw/camfit/config/CorsConfig.java
+++ src/main/java/kr/hs/dgsw/camfit/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
       config.addAllowedHeader("*");
       config.addAllowedMethod("*");
 
-      source.registerCorsConfiguration("/member/**", config);
+      source.registerCorsConfiguration("/**", config);
 
       return new CorsFilter(source);
    }
