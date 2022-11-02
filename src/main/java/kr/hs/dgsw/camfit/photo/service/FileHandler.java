c19a311e9f703e352eaef27d3499c1c496ca7571
seojun-kim <80928795+seojun-kim@users.noreply.github.com>
2022-11-02 20:25:16 +0900

    filehanlder path C:\fileUpload\images 수정
    fileUpload\\images\\" + uuid

--- src/main/java/kr/hs/dgsw/camfit/photo/service/FileHandler.java
+++ src/main/java/kr/hs/dgsw/camfit/photo/service/FileHandler.java
@@ -30,22 +30,23 @@ public class FileHandler {
 
         // 전달되어 온 파일이 존재할 경우
         if(!CollectionUtils.isEmpty(multipartFiles)) {
-            System.out.println("============================== fileHandler");
             // 파일명을 UUID로 저장
             UUID uuid = UUID.randomUUID();
 
             // 프로젝트 디렉터리 내의 저장을 위한 절대 경로 설정
             // 경로 구분자 File.separator 사용
-            String absolutePath = new File("").getAbsolutePath() + File.separator + File.separator;
+            //String absolutePath = new File("C:") + File.separator;
 
             // 파일을 저장할 세부 경로 지정
-            //"src" + File.separator + "main" + File.separator + "resources" + File.separator + "static" + File.separator + "images"
-            String path = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "static" + File.separator + "images" + File.separator + uuid;
+            // "fileUpload" + File.separator + "images" + File.separator + uuid
+            String path = "C:\\fileUpload\\images\\" + uuid;
             File file = new File(path);
 
             // 디렉터리가 존재하지 않을 경우
             if(!file.exists()) {
+                System.out.println("================================== 디렉터리 생성");
                 boolean wasSuccessful = file.mkdirs();
+                System.out.println(wasSuccessful);
 
                 // 디렉터리 생성에 실패했을 경우
                 if(!wasSuccessful) {
@@ -62,7 +63,6 @@ public class FileHandler {
 
                 //확장명이 존재하지 않을 경우 X
                 if(ObjectUtils.isEmpty(contentType)) {
-                    System.out.println("확장명이 없음");
                     break;
                 } else { // 확장자가 jpeg, png인 파일들만 받아서 처리
                     if(contentType.equals("image/jpeg")) {
@@ -70,7 +70,6 @@ public class FileHandler {
                     } else if(contentType.equals("image/png")) {
                         originalFileExtension = ".png";
                     } else { //다른 확장자인 경우 처리 X
-                        System.out.println("다른 확장자");
                         break;
                     }
                 }
@@ -100,9 +99,8 @@ public class FileHandler {
                 // 생성 후 리스트에 추가
                 fileList.add(photo);
 
-                System.out.println("===================== 파일 저장 전");
                 // 업로드 한 파일 데이터를 지정한 파일에 저장
-                file = new File(absolutePath + path + File.separator + newFileName);
+                file = new File(path + File.separator + newFileName);
                 multipartFile.transferTo(file);
 
                 // 파일 권한 설정(쓰기, 읽기)
